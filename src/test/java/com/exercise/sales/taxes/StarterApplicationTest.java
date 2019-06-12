f68715fdd6181e30565828c4224e005978137842
BarrottaDario <dario@earlymorning.com>
2019-06-12 23:47:24 +0200

    First Commit - Working empty project with basic setup

---
+++ src/test/java/com/exercise/sales/taxes/StarterApplicationTest.java
@@ -0,0 +1,14 @@
+package com.exercise.sales.taxes;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@EnableAutoConfiguration
+@SpringBootTest
+public class StarterApplicationTest {
+
+    @Test
+    public void main() {
+    }
+}
